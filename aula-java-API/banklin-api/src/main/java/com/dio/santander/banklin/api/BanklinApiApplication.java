696d3a225edaabefdff03cc3758ecd7152884229
HilarioLelis <hilariolelis@gmail.com>
2022-05-03 21:12:23 -0300

    Adicionando o spring ao projeto
    primeiro commit

---
+++ aula-java-API/banklin-api/src/main/java/com/dio/santander/banklin/api/BanklinApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banklin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklinApiApplication.class, args);
+	}
+
+}
